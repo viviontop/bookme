67b475f20d84de810e43db2a1a3367b5e77f449c
viviontop <becheruandrei9@gmail.com>
2025-12-31 19:09:15 +0200

    nununu

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,9 +5,7 @@ declare global {
   var __prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.__prisma ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL
-});
+const prisma = globalThis.__prisma ?? new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalThis.__prisma = prisma;
 
 export default prisma;
